d3465b6c6015bd6a98488a0e9a4ca1b5c14e4646
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-11-29 14:20:39 +0900

    Prisma service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // DTO, class Validationの有効化
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // BE←FEのアクセス許可
+  app.enableCors({
+    // cookieベースでの通信を有効化
+    credentials: true,
+    origin: ['https://localhost:3000'],
+  });
+  // middlewareでcookieの解析を有効化
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
